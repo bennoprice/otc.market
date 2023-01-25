d686b3b2b5f217fb5ecb0dbd7bf740670190ae76
Ben Price <ben.jules.price@gmail.com>
2023-01-25 22:04:23 +0000

    update

--- frontend/src/Home/index.js
+++ frontend/src/Home/index.js
@@ -1,19 +1,8 @@
-import Heading from '../Heading';
 import './style.css';
 
 const Home = () => (
 	<section id="home">
-      <div>
-         <h1><span className="highlight">B</span>en</h1>
-         <h1><span className="highlight">P</span>rice</h1>
-      </div>
-		<div>
-			<Heading title='about' />
-         <p>
-				I've been interested in -and learning Computer Science from the age of 12.
-            I consider myself very versatile and have a passion for learning new technologies. 
-         </p>
-      </div>
+      <h1>Home</h1>
    </section>
 );
 
